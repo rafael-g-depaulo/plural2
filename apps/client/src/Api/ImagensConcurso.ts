60f8b98ceb2e9976e604867a061da286cf31d50e
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-08-02 21:09:28 -0300

    ✨ Create connection to Strapi imagens concurso

---
+++ apps/client/src/Api/ImagensConcurso.ts
@@ -0,0 +1,42 @@
+import { strapi } from "Api"
+import useFetchApi from 'Hooks/useFetchApi'
+import { Asset2Image, Image } from "Utils/Image"
+import StrapiAsset from "Utils/StrapiAsset"
+
+interface ImagemConcursoStrapi {
+  id: number
+  titulo: string
+  nome_autor: string
+  idade_autor: number
+  foto: StrapiAsset
+}
+
+export interface ImagemConcurso {
+  id: number
+  titulo: string
+  nome_autor: string
+  idade_autor: number
+  foto: Image
+}
+
+const transformImage = ({ foto, id, idade_autor, nome_autor, titulo }: ImagemConcursoStrapi) => ({
+  id,
+  idade_autor,
+  nome_autor,
+  titulo,
+  foto: Asset2Image(foto)
+}) as ImagemConcurso
+
+export const fetchImagemConcurso = (id: number) => strapi
+  .get<ImagemConcursoStrapi>(`/imagem-concursos/${id}`)
+  .then(({ data }) => data)
+  .then(transformImage)
+
+export const useImagemConcurso = (id: number) => useFetchApi<ImagemConcurso>(`/imagem-concursos/${id}`, () => fetchImagemConcurso(id))
+// 
+export const fetchAllImagemConcurso = () => strapi
+  .get<ImagemConcursoStrapi[]>(`/imagem-concursos`)
+  .then(({ data }) => data)
+  .then(imagens => imagens.map(transformImage))
+
+export const useAllImagemConcurso = () => useFetchApi<ImagemConcurso[]>(`/imagem-concursos`, fetchAllImagemConcurso)
